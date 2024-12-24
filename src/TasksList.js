f4c54e98de5ebb4d5a79e6aee4da83f78d579fb0
DavidAndBar <davidbar22@hotmail.com>
2024-12-24 02:12:09 -0500

    fixes display message when no items in the list

--- src/TasksList.js
+++ src/TasksList.js
@@ -6,13 +6,19 @@ const TasksList = ({ activeList, setActiveList, columnCompleted, search }) => {
       return <Task key={item.id} activeList={activeList} setActiveList={setActiveList} item={item} columnCompleted={columnCompleted}/>
     } 
   })
+
+  let taskCount = 0;
+  tasks.forEach( (el) => {
+    if (typeof(el) === "object") {taskCount +=1};
+  })
   
+
   return (
     <section>
       <div>
         <h3>{columnCompleted ?  "Done" : "Pending" }</h3>
       </div>
-      {tasks.length !== 0 ? tasks : <p style={{textAlign:"center"}}>No tasks here</p>}
+      {taskCount !== 0 ? tasks : <p style={{textAlign:"center"}}>No tasks here</p>}
     </section>
   )
 }
